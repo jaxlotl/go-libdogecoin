c3bb490a33241a2ad0dcc46bbbe3a6fe53151498
jaxlotl <jackie.mcaninch.2019@gmail.com>
2022-07-15 14:01:10 -0700

    updating function descriptors

--- include/libdogecoin.h
+++ include/libdogecoin.h
@@ -1,5 +1,29 @@
-/* libdogecoin.h v0.1 - (c)2022 The Dogecoin Foundation */
-/* need licensestatement here */
+/*
+
+ The MIT License (MIT)
+
+ Copyright (c) 2022 bluezr
+ Copyright (c) 2022 The Dogecoin Foundation
+
+ Permission is hereby granted, free of charge, to any person obtaining
+ a copy of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ and/or sell copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included
+ in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
+
+ */
 
 #include <limits.h>
 #include <stdint.h>
@@ -46,14 +70,14 @@ int start_transaction();
 int add_utxo(int txindex, char* hex_utxo_txid, int vout);
 
 /* add an output to the transaction being worked on at (txindex) of amount (amount) in dogecoins, returns 1 if successful. */
-int add_output(int txindex, char* destinationaddress, long double amount);
+int add_output(int txindex, char* destinationaddress, char* amount);
 
 /* finalize the transaction being worked on at (txindex), with the (destinationaddress) paying a fee of (subtractedfee), */
 /* re-specify the amount in dogecoin for verification, and change address for change. If not specified, change will go to the first utxo's address. */
-char* finalize_transaction(int txindex, char* destinationaddress, long double subtractedfee, long double out_dogeamount_for_verification, char* public_key);
+char* finalize_transaction(int txindex, char* destinationaddress, char* subtractedfee, char* out_dogeamount_for_verification, char* changeaddress);
 
 /* sign a raw transaction in memory at (txindex), sign (inputindex) with (scripthex) of (sighashtype) for (amount - koinu? dogecoin? why a 3rd time), with (privkey) */
-int sign_transaction(int txindex, long double amounts[], char* script_pubkey, char* privkey);
+int sign_transaction(int txindex, char* amounts[], char* script_pubkey, char* privkey);
 
 /* clear all internal working transactions */
 void remove_all();
@@ -69,7 +93,7 @@ void clear_transaction(int txindex);
 */
 
 /*Sign a raw transaction hexadecimal string using inputindex, scripthex, sighashtype, amount and privkey. */
-int sign_raw_transaction(int inputindex, char* incomingrawtx, char* scripthex, int sighashtype, long double amount, char* privkey);
+int sign_raw_transaction(int inputindex, char* incomingrawtx, char* scripthex, int sighashtype, char* amount, char* privkey);
 
 /*Store a raw transaction that's already formed, and give it a txindex in memory. (txindex) is returned as int. */
 int store_raw_transaction(char* incomingrawtx);
